cce2481aa73f3689726485e7bcb9681e9d670396
AkritiUpadhya13 <104682788+AkritiUpadhya13@users.noreply.github.com>
2022-04-30 21:23:09 +0530

    Create ex2.m

---
+++ Exercise2/Ex2/ex2.m
@@ -0,0 +1,38 @@
+data=load('ex2data1.txt');
+X=data(:,[1,2]);
+y=data(:,3);
+pos=find(y==1);
+neg=find(y==0);
+plotData(X,y);
+[m,n]=size(X);
+X=[ones(m,1) X];
+
+initial_theta=zeros(n+1,1);
+[cost,grad]=costFunction(initial_theta,X,y);
+fprintf('cost at initial_theta(zeros):%f\n',cost);
+disp('gradient at initial_theta(zeros):');
+disp('grad');
+
+test_theta=[-24;0.2;0.2];
+[cost,grad]=costFunction(test_theta,X,y);
+fprintf('cost at non-zero test theta: %f\n', cost);
+disp('gradient at non-zero test theta:');
+disp('grad');
+
+options=optimoptions(@fminunc, 'Algorithm','Quasi-Newton','GradObj', 'on', 'MaxIter', 400);
+[theta, cost]= fminunc(@(t)(costFunction(t,X,y)), initial_theta, options);
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+disp('theta:');
+
+plotDecisionBoundary(theta,X,y);
+hold on;
+xlabel('Exam1 score');
+ylabel('Exam2 score');
+legend('Admitted', 'Not admitted');
+
+prob=sigmoid([1 45 85]*theta);
+fprintf('for a student with scores 45 and 85, we predict an admission probability of %f\n\n', prob);
+p=predict(theta,X);
+fprintf('Train accuracy:%f\n', mean(double(p==y))*100)
+
+
