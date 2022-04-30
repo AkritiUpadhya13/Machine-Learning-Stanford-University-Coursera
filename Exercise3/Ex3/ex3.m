307e9c68475c74464395925156f54f352c2b82bd
AkritiUpadhya13 <104682788+AkritiUpadhya13@users.noreply.github.com>
2022-04-30 21:35:12 +0530

    Create ex3.m

---
+++ Exercise3/Ex3/ex3.m
@@ -0,0 +1,13 @@
+load ('ex3data1.mat');
+m=size(X,1);
+sel=randperm(size(X,1));
+sel=sel(1:100);
+displayData(X(sel,:));
+load('ex3weights.mat');
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+rp=randi(m);
+pred=predict(Theta1,Theta2,X(rp,:));
+fprintf('\n Neural network prediction:%d (digit %d) \n', pred, mod(pred,10));
+displayData(X(rp,:));
+
